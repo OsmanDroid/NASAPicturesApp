3bb45c9779541da7efb4b98b69eda59804cafcdf
Osman <osmanjaber786@gmail.com>
2022-07-07 17:02:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/osmandroid/nasapicturesapp/ui/DetailScreenFragment.kt
@@ -0,0 +1,45 @@
+package com.osmandroid.nasapicturesapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.osmandroid.nasapicturesapp.R
+import com.osmandroid.nasapicturesapp.databinding.FragmentDetailScreenBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class DetailScreenFragment : Fragment() {
+
+    private var _binding: FragmentDetailScreenBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentDetailScreenBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/osmandroid/nasapicturesapp/ui/GridScreenFragment.kt
@@ -0,0 +1,45 @@
+package com.osmandroid.nasapicturesapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.osmandroid.nasapicturesapp.R
+import com.osmandroid.nasapicturesapp.databinding.FragmentGridScreenBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class GridScreenFragment : Fragment() {
+
+    private var _binding: FragmentGridScreenBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentGridScreenBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
